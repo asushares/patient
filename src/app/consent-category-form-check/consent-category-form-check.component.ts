410f6db736418eb01b573372e75a21f2686ccca6
Fred Hong <frederickhong.5@gmail.com>
2024-04-08 20:48:32 -0700

    add icon element to form check component

--- src/app/consent-category-form-check/consent-category-form-check.component.ts
+++ src/app/consent-category-form-check/consent-category-form-check.component.ts
@@ -1,17 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Popover } from 'bootstrap';
 
 @Component({
   selector: 'app-consent-category-form-check',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './consent-category-form-check.component.html',
-  styleUrl: './consent-category-form-check.component.scss'
+  styleUrl: './consent-category-form-check.component.scss',
 })
-export class ConsentCategoryFormCheckComponent {
+export class ConsentCategoryFormCheckComponent implements AfterViewInit {
   @Input() category!: {
     id: string;
     label: string;
   };
   @Input() isFirst!: boolean;
+
+  ngAfterViewInit() {
+    // Initialize Bootstrap popover
+    const popoverIcon = document.querySelector(`#${this.category.id} ~ i`);
+    new Popover(popoverIcon!);
+    // Hide all other popovers when one is shown
+    popoverIcon?.addEventListener('show.bs.popover', () => {
+      const allOtherIcons = document.querySelectorAll(
+        `.form-check-input:not(#${this.category.id}) ~ i`
+      );
+      allOtherIcons.forEach((icon) => {
+        const popover = Popover.getInstance(icon);
+        popover?.hide();
+      });
+    });
+  }
 }
