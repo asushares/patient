e705b3537cf58d7fdc662dd0401eb14cafcc7d99
Fred Hong <frederickhong.5@gmail.com>
2024-04-13 08:02:17 -0700

    1. use example json as request body; 2. content in popover could be updated dynamically

--- src/app/cds.service.ts
+++ src/app/cds.service.ts
@@ -1,41 +1,40 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
 import { HttpHeaders } from '@angular/common/http';
-import { NoConsentCard, PatientConsentHookRequest } from '@asushares/core';
-
-console.log(NoConsentCard, PatientConsentHookRequest);
+import { BehaviorSubject } from 'rxjs';
+import { NoConsentCard } from '@asushares/core';
 
 type RequestType =
-  | ['get', 'root']
-  | ['get', 'discovery']
-  | ['post', 'hook']
-  | ['get', 'request_schema']
-  | ['get', 'response_schema']
-  | ['get', 'sensitivity_rules']
-  | ['post', 'sensitivity_rules'];
+  | 'get-root'
+  | 'get-discovery'
+  | 'post-hook'
+  | 'get-request_schema'
+  | 'get-response_schema'
+  | 'get-sensitivity_rules'
+  | 'post-sensitivity_rules';
 
 type EndpointType = {
   url: string;
   headers: string[];
 };
 
-const cdsEndpoints = new WeakMap<RequestType, EndpointType>([
+const cdsEndpoints = new Map<RequestType, EndpointType>([
   [
-    ['get', 'root'],
+    'get-root',
     {
       url: '/',
       headers: ['standard'],
     },
   ],
   [
-    ['get', 'discovery'],
+    'get-discovery',
     {
       url: '/cds-services',
       headers: ['standard'],
     },
   ],
   [
-    ['post', 'hook'],
+    'post-hook',
     {
       url: '/cds-services/patient-consent-consult',
       headers: [
@@ -46,28 +45,28 @@ const cdsEndpoints = new WeakMap<RequestType, EndpointType>([
     },
   ],
   [
-    ['get', 'request_schema'],
+    'get-request_schema',
     {
       url: '/schemas/patient-consent-consult-hook-request.schema.json',
       headers: ['standard'],
     },
   ],
   [
-    ['get', 'response_schema'],
+    'get-response_schema',
     {
       url: '/schemas/patient-consent-consult-hook-response.schema.json',
       headers: ['standard'],
     },
   ],
   [
-    ['get', 'sensitivity_rules'],
+    'get-sensitivity_rules',
     {
       url: '/data/sensitivity-rules.json',
       headers: ['standard'],
     },
   ],
   [
-    ['post', 'sensitivity_rules'],
+    'post-sensitivity_rules',
     {
       url: '/data/sensitivity-rules.json',
       headers: ['standard'],
@@ -79,7 +78,11 @@ const cdsEndpoints = new WeakMap<RequestType, EndpointType>([
   providedIn: 'root',
 })
 export class CDSService extends BaseService {
-  public requestCreator<T extends string>(requestType: RequestType) {
+  public current = new BehaviorSubject<NoConsentCard | null>(null);
+
+  public requestCreator<T extends string, S extends object>(
+    requestType: RequestType,
+  ) {
     const endpoint = cdsEndpoints.get(requestType);
     if (!endpoint) {
       throw new Error('Invalid request type');
@@ -91,7 +94,7 @@ export class CDSService extends BaseService {
 
     type ParameterTypes = {
       body?: object;
-    } & Record<T, string>;
+    } & Partial<Record<T, string>>;
 
     const request = (parameters: ParameterTypes) => {
       let headers = new HttpHeaders();
@@ -100,19 +103,49 @@ export class CDSService extends BaseService {
         headers = headers.set('Accept', 'application/json');
       }
       otherHeaders.forEach(h => {
-        headers = headers.set(h, parameters[h]);
+        if (typeof parameters[h] === 'string' && parameters[h] !== '') {
+          headers = headers.set(h, parameters[h]!);
+        }
       });
-      if (requestType[0] === 'get') {
-        return this.http.get(url, { headers });
+      if (requestType.split('-')[0] === 'get') {
+        return this.http.get<S>(url, { headers });
       } else {
-        return this.http[requestType[0]](
-          url,
-          JSON.stringify(parameters?.body ?? {}),
-          { headers },
-        );
+        return this.http.post<S>(url, JSON.stringify(parameters?.body ?? {}), {
+          headers,
+        });
       }
     };
 
     return request;
   }
+
+  public getRoot = this.requestCreator('get-root');
+
+  public getDiscovery = this.requestCreator('get-discovery');
+
+  public postHook = (
+    parameters: { body: object } & Partial<
+      Record<'cds-confidence-threshold' | 'cds-redaction-enabled', string>
+    >,
+  ) =>
+    this.requestCreator<
+      'cds-confidence-threshold' | 'cds-redaction-enabled',
+      NoConsentCard
+    >('post-hook')(parameters).subscribe({
+      next: d => {
+        this.current.next(d);
+      },
+      error: e => {
+        console.error('Error posting hook.');
+        console.error(e);
+      },
+    });
+
+  public getRequestSchema = this.requestCreator('get-request_schema');
+
+  public getResponseSchema = this.requestCreator('get-response_schema');
+
+  public getSensitivityRules = this.requestCreator('get-sensitivity_rules');
+
+  public postSensitivityRules = this.requestCreator('post-sensitivity_rules');
 }
